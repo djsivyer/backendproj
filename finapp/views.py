15399665c61eab7f96f20d1867b72f55735c5b3d
djsivyer <u1112036@umail.usq.edu.au>
2023-06-04 13:02:16 +1000

    AI gen'd code, to be checked

--- finapp/views.py
+++ finapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def login_view(request):
+    if request.method == 'POST':
+        # Handle login form submission
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Implement your login logic here
+
+    return render(request, 'login.html')
