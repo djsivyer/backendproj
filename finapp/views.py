64207b17b58e4f77c3a27748df7ce661610d4de7
djsivyer <u1112036@umail.usq.edu.au>
2023-06-04 14:49:11 +1000

    Changed function names

--- finapp/views.py
+++ finapp/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from django.views import generic
 # Create your views here.
 
-def index_view(request):
+def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
