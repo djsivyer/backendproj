e30efbe62d9a03fc65d5268b119ddb9edcf95886
djsivyer <u1112036@umail.usq.edu.au>
2023-06-05 20:22:19 +1000

    add register request

--- finapp/views.py
+++ finapp/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.template import loader
-from django.views import generic
+#from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+#from django.template import loader
+#from django.views import generic
 # Create your views here.
 
 def index_view(request):
@@ -12,3 +15,15 @@ def login_view(request):
 
 def register_view(request):
     return render(request, 'finapp/register.html')
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="main/register.html", context={"register_form":form})
