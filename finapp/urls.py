8e4e6d84e0cc9cea2db5bb5c476bbf8b7324bbfb
djsivyer <u1112036@umail.usq.edu.au>
2023-06-05 19:25:38 +1000

    Rolled back some changes

--- finapp/urls.py
+++ finapp/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 app_name = "finapp"
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("logon/", views.logon_view, name="logon"),
 ]
