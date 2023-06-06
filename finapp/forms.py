b5d5c6c5a850fe17794c316a99b1f9c0c4a9b1dd
djsivyer <u1112036@umail.usq.edu.au>
2023-06-06 18:49:46 +1000

    Removed Check

--- finapp/forms.py
+++ finapp/forms.py
@@ -40,7 +40,6 @@ class RegisterForm(UserCreationForm):
         help_text='Enter Password Again',
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}),
     )
-    check = forms.BooleanField(required=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'check']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
