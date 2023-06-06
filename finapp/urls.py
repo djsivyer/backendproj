c6eb00705f39056f197c607c1724e0caeca17875
djsivyer <u1112036@umail.usq.edu.au>
2023-06-06 10:34:00 +1000

    added homepage

--- finapp/urls.py
+++ finapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index_view, name="index"),
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
+    path("homepage/", views.homepage, name="homepage"),
 ]
