bbefdc59c3644e9a2cd120342846f0ae33b4d159
djsivyer <davidjsivyer@gmail.com>
2023-06-19 14:14:15 +1000

    Login-out views working as intended

--- finapp/urls.py
+++ finapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index_view, name="index"),
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
-    path("home", views.login_view, name="homepage")
+    path("homepage/", views.home_view, name="homepage"),
+    path("logout", views.logout_view, name="logout")
 ]
--- finapp/views.py
+++ finapp/views.py
@@ -1,7 +1,7 @@
 #from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 #from django.template import loader
 #from django.views import generic
@@ -10,6 +10,12 @@ from django.contrib import messages
 
 def index_view(request):
     return render(request, 'finapp/index.html')
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'finapp/homepage.html')
+    else:
+        return redirect('finapp:index')
 
 def login_view(request):
     if request.method == 'GET':
@@ -21,11 +27,13 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('finapp:homepage.html')
+            return redirect('finapp:homepage')
         else:
             messages.error(request, "Unsuccessful login. Invalid information.")
 
 def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('finapp:homepage')
     if request.method == 'GET':
         form = RegisterForm()
         context = {'form': form}
@@ -37,9 +45,13 @@ def register_view(request):
             form.save()
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user)
-            return redirect('finapp:homepage.html')
+            return redirect('finapp:homepage')
         else:
             print('Form is not valid')
             messages.error(request, 'Error Processing Your Request')
         context = {'form': form}
         return render(request, 'finapp/register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('finapp:index')
