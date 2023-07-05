537d4ef7cc9fc776b2a143a8b0a1e97593598d48
djsivyer <davidjsivyer@gmail.com>
2023-07-06 07:09:32 +1000

    Removed unrequired import

--- finapp/views.py
+++ finapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from finapp.forms import RegisterForm
